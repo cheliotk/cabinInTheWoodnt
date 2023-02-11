c6a3d747ae7e03b3e927207bf11e19f55453ad12
Kostas Cheliotis <cheliotis.k@gmail.com>
2023-02-11 11:54:51 +0200

    adding paragraph text support

--- Assets/Scripts/UI/TextController.cs
+++ Assets/Scripts/UI/TextController.cs
@@ -10,6 +10,34 @@ public class TextController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI acknowledgementText;
     [SerializeField] private int maxTextsListCount = 50;
     private List<GameObject> textsList = new List<GameObject>();
+    private List<string> currentDescriptionTexts = new List<string>();
+    private int currentDescriptionTextsCounter = 0;
+
+    public void SetCurrentDescriptionsText(List<string> texts)
+    {
+        currentDescriptionTexts = texts;
+        currentDescriptionTextsCounter = 0;
+    }
+
+    public bool HasMoreText()
+    {
+        return currentDescriptionTextsCounter < currentDescriptionTexts.Count;
+    }
+
+    public void ShowNextText(TextType textType)
+    {
+        string text = currentDescriptionTexts[currentDescriptionTextsCounter];
+        var entry = Instantiate(textEntryPrefab, textContainer);
+        entry.GetComponent<TextEntry>().SetText(text, TextType.STANDARD);
+        textsList.Add(entry);
+
+        if (textsList.Count > maxTextsListCount)
+        {
+            RemoveFirstEntry();
+        }
+
+        currentDescriptionTextsCounter++;
+    }
     
     public void AddText(string text)
     {
@@ -19,19 +47,12 @@ public class TextController : MonoBehaviour
 
         if(textsList.Count > maxTextsListCount)
         {
-            GameObject lastObject = textsList[0];
-            Destroy(lastObject);
-            textsList.RemoveAt(0);
+            RemoveFirstEntry();
         }
     }
 
     public void AddAcknowledgementText(string text)
     {
-        //AddText(text);
-
-        //acknowledgementText.text = text + 
-        //    "\nWhat you want me to do next?";
-
         var entry = Instantiate(textEntryPrefab, textContainer);
         entry.GetComponent<TextEntry>().SetText(text, TextType.ACKNOWLEDGEMENT);
         textsList.Add(entry);
@@ -50,9 +71,7 @@ public class TextController : MonoBehaviour
 
         if (textsList.Count > maxTextsListCount)
         {
-            GameObject lastObject = textsList[0];
-            Destroy(lastObject);
-            textsList.RemoveAt(0);
+            RemoveFirstEntry();
         }
     }
 
